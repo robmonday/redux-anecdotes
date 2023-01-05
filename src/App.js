ae909336e564b2137223f52cdebeacd78ff75eff
“robmonday” <10903531+robmonday@users.noreply.github.com>
2023-01-05 17:48:55 -0500

    exercise 6.5 completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { createVoteAction, createAnecdoteAction } from './reducers/anecdoteReduc
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = anecdotes.sort((a,b) => b.votes - a.votes)
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -20,7 +22,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
